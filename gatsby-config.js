e5f26c9d40d1680983fe9056914a3b19640a6990
Sailesh Kasaju <leosailesh@gmail.com>
2019-10-06 20:03:21 +0545

    update metadata of site and add a demo project

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,28 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Emilia - Gatsby Starter Portfolio`,
+    siteTitle: `AnmolVajracharya`,
+    siteTitleAlt: `Anmol Bajracharya - My Portfolio`,
+    siteHeadline: `Emilia - Gatsby Theme from @lekoarts`,
+    siteUrl: `https://www.anmolvajracharya.tk`,
+    // Used for SEO
+    siteDescription: `Portfolio/photography site of Anmol Bajracharya.`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@anmol_vajracharya`
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-emilia`,
-      options: {},
+      options: {
+        name: 'anmol_vajracharya',
+        location: 'Nepal',
+        socialMedia: [{ title: `Instagram`, href: `https://www.instagram.com/anmol_vajracharya/` }],
+        showThemeAuthor: false
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
@@ -21,9 +37,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Emilia - @lekoarts/gatsby-theme-emilia`,
-        short_name: `Emilia`,
-        description: `Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Themeable with Theme UI.`,
+        name: `AnmolVajracharya - @anmol_vajracharya`,
+        short_name: `AnmolVajracharya`,
+        description: `Portfolio/photography site of Anmol Bajracharya.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#3182ce`,
