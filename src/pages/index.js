819077c0c465b590dd2d09a57044b3b885f8b40e
Sailesh Kasaju <leosailesh@gmail.com>
2019-10-11 14:02:33 +0545

    remove ids

--- src/pages/index.js
+++ src/pages/index.js
@@ -142,25 +142,14 @@ class HomeIndex extends React.Component {
                 >
                   <div className="row uniform 50%">
                     <div className="6u 12u$(xsmall)">
-                      <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        placeholder="Name"
-                      />
+                      <input type="text" name="name" placeholder="Name" />
                     </div>
                     <div className="6u 12u$(xsmall)">
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="Email"
-                      />
+                      <input type="email" name="email" placeholder="Email" />
                     </div>
                     <div className="12u">
                       <textarea
                         name="message"
-                        id="message"
                         placeholder="Message"
                         rows="4"
                       ></textarea>
