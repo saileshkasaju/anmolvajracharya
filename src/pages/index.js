34b07fc479a59588bf686bfd1adf62aa17e61fbd
Sailesh Kasaju <leosailesh@gmail.com>
2019-10-11 13:54:43 +0545

    form submittion fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -161,12 +161,12 @@ class HomeIndex extends React.Component {
                       ></textarea>
                     </div>
                   </div>
+                  <ul className="actions">
+                    <li>
+                      <input type="submit" value="Send Message" />
+                    </li>
+                  </ul>
                 </form>
-                <ul className="actions">
-                  <li>
-                    <button type="submit">Send Message</button>
-                  </li>
-                </ul>
               </div>
               <div className="4u 12u$(small)">
                 <ul className="labeled-icons">
