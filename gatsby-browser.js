6c63f9f12409b952cb43fe1280cf04d7277fad5d
Sailesh Kasaju <leosailesh@gmail.com>
2019-11-21 18:25:24 +0545

    readd code for disabling rightclick

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,17 +11,17 @@ import 'typeface-merriweather'
 import 'typeface-nunito'
 import 'typeface-alegreya'
 
-// function click(e) {
-//   if (!e) e = window.event
-//   if (
-//     (e.type && e.type === 'contextmenu') ||
-//     (e.button && e.button === 2) ||
-//     (e.which && e.which === 3)
-//   ) {
-//     if (window.opera) window.alert('')
-//     return false
-//   }
-// }
-// if (document.layers) document.captureEvents(Event.MOUSEDOWN)
-// document.onmousedown = click
-// document.oncontextmenu = click
+function click(e) {
+  if (!e) e = window.event
+  if (
+    (e.type && e.type === 'contextmenu') ||
+    (e.button && e.button === 2) ||
+    (e.which && e.which === 3)
+  ) {
+    if (window.opera) window.alert('')
+    return false
+  }
+}
+if (document.layers) document.captureEvents(Event.MOUSEDOWN)
+document.onmousedown = click
+document.oncontextmenu = click
